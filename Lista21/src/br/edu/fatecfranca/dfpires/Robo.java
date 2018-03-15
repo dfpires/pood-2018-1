4c6f9ffd7bbb542c4bf1c529e3f30ad199a2bda4
dfpires <dfpires@gmail.com>
2018-03-15 12:54:32 -0300

    Add files via upload

---
+++ Lista21/src/br/edu/fatecfranca/dfpires/Robo.java
@@ -0,0 +1,66 @@
+package br.edu.fatecfranca.dfpires;
+public class Robo {
+    private String movimento; // frente, trás, direita, esquerda, parado
+    private int vel; // 0, 1, 2, 3
+    
+    public Robo(){
+        this.movimento = "parado";
+        this.vel = 0;
+    }
+    public Robo(String movimento, int vel){
+        trataMovimento(movimento);
+        trataVel(vel);
+    }
+    public void movimentarFrente(){
+        this.movimento = "frente";
+    }
+    public void movimentarTraz(){
+        this.movimento = "traz";
+    }
+    public void movimentarDireita(){
+        this.movimento = "direita";
+    }
+    public void movimentarEsquerda(){
+        this.movimento = "esquerda";
+    }
+    private void trataMovimento(String movimento){
+        switch(movimento) {
+            case "frente": this.movimento = movimento;
+                        break;
+            case "tras": this.movimento = movimento;
+                        break; 
+            case "direita": this.movimento = movimento;
+                        break;
+            case "esquerda": this.movimento = movimento;
+                        break;            
+            case "parado": this.movimento = movimento;
+                        break;                        
+            default: System.out.println("Movimento não permitido ");// não é alterada;
+        }
+    }
+    public void setMovimento(String movimento){
+        this.trataMovimento(movimento);
+    }
+    public String getMovimento(){
+        return this.movimento;
+    }
+    public int getVel(){
+        return this.vel;
+    }
+    private void trataVel(int vel){
+         switch(vel) {
+            case 0: this.vel = vel;
+                        break;
+            case 1: this.vel = vel;
+                        break; 
+            case 2: this.vel = vel;
+                        break;
+            case 3: this.vel = vel;
+                        break;            
+            default: System.out.println("Velocidade não permitido ");// não é alterada;
+        }
+    }
+    public void setVel(int vel){
+        trataVel(vel);
+    }
+}
---
+++ Lista2Testa/src/br/edu/fatecfranca/TestaCliente.java
@@ -0,0 +1,13 @@
+package br.edu.fatecfranca;
+import br.edu.fatecfranca.dfpires.exe1.Cliente;
+public class TestaCliente {
+    public static void main(String[] args) {
+        Cliente cli1 = new Cliente();
+        
+        Cliente cli2 = new Cliente("234566-7", "4565-4", "Marcos", 1000);
+        
+        cli2.
+        
+    }
+    
+}
