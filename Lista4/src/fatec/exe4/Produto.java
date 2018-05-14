1380b0c9762c430d5e1bc22f9a6388597d5c258d
dfpires <dfpires@gmail.com>
2018-05-14 09:15:12 -0300

    Add files via upload

--- Lista4/src/fatec/exe4/Produto.java
+++ Lista4/src/fatec/exe4/Produto.java
@@ -1,4 +1,5 @@
 package fatec.exe4;
+import java.util.Random;
 public class Produto {
     protected int serial, volume;
     protected String teste;
@@ -10,35 +11,28 @@ public class Produto {
     public Produto(int serial, int volume) {
         this.serial = serial;
         this.volume = volume;
+        this.teste = "não testado";
     }
-
-    public int getSerial() {
-        return serial;
+    public int getSerial() {      return serial;   }
+    public void setSerial(int serial) {        this.serial = serial;    }
+    public int getVolume() {        return volume;    }
+    public void setVolume(int volume) {        this.volume = volume;    }
+    public String getTeste() {        return teste;    }
+    public void setTeste(String teste) {        testaUnidade();    }
+    public boolean testaUnidade(){
+        // 90% de chance do produto ser aprovado, 10% de reprovado
+        if (this.teste.equals("não testado")){
+            Random obj = new Random();
+            int r = obj.nextInt(10);
+            if (r < 9){
+                this.teste = "aprovado"; return true;
+            }
+            else {
+                this.teste = "reprovado"; return false;
+            }
+        }
+        else return false;
     }
-
-    public void setSerial(int serial) {
-        this.serial = serial;
-    }
-
-    public int getVolume() {
-        return volume;
-    }
-
-    public void setVolume(int volume) {
-        this.volume = volume;
-    }
-
-    public String getTeste() {
-        return teste;
-    }
-
-    public void setTeste(String teste) {
-        testaUnidade(teste);
-    }
-    public boolean testaUnidade(String teste){
-        return false;
-    }
-
     @Override
     public String toString() {
         return "Produto{" + "serial=" + serial + ", volume=" + volume + ", teste=" + teste + '}';
--- Lista4/src/fatec/exe4/Testa.java
+++ Lista4/src/fatec/exe4/Testa.java
@@ -1,7 +1,8 @@
 package fatec.exe4;
 public class Testa {
-    public static void polimorfico(Produto produto){
-        System.out.println(produto.toString());
+    public static void polimorfico(Produto camaleao){
+        // cameleao ora executa toString() da Radio, ora da Tv
+        System.out.println(camaleao.toString()); //polimorfismo
     }
     public static void main(String[] args) {
         Radio radio = new Radio("FM", 96.5f, 123, 456);
